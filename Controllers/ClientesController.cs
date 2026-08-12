74e47c5d49c6ad9f59455a0e0ad5ac59529d7181
MANSJR13 <m.aureliosilvajr@outlook.com.br>
2026-08-11 22:58:44 -0300

    Criação da tela de clientes e inicio do desenvolvimento do backend de cadastro de clientes

---
+++ Controllers/ClientesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebBarber.Controllers
+{
+    public class ClientesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Criar()
+        {
+            return View();
+        }
+        public IActionResult Editar()
+        {
+            return View();
+        }
+        public IActionResult ApagarConfirmar()
+        {
+            return View();
+        }
+
+    }
+}
